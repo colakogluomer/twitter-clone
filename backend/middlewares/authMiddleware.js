7913587b3101fa36197ce3d196e98f91f4aa37d1
colakogluomer <colakogluomer00@gmail.com>
2021-08-22 13:52:13 +0300

    Add authentication process

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,15 +1,13 @@
 const jwt = require("jsonwebtoken");
-const ApiError = require("../utils/ApiError");
 
 const auth = async (req, res, next) => {
-  console.log("hiii");
   const token = req.headers.authorization.split(" ")[1];
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized!" });
     }
-    req.userTckn = decoded.userTckn;
+    req._id = decoded._id;
     next();
   });
 };
--- backend/services/authService.js
+++ backend/services/authService.js
@@ -9,26 +9,54 @@ const register = async (user) => {
   if (existingUser) throw new ApiError(404, "User already exist!");
 
   const hashedPassword = await bcrypt.hash(user.password, 10);
+  const newUser = null;
+  if (user.email) {
+    newUser = await User.create({
+      name: user.name,
+      username: user.username,
+      password: hashedPassword,
+      email: user.email,
+      birthDate: user.birthDate,
+    });
+  } else {
+    newUser = await User.create({
+      name: user.name,
+      username: user.username,
+      password: hashedPassword,
+      phone: user.phone,
+      birthDate: user.birthDate,
+    });
+  }
 
-  const newUser = await User.create({
-    name: user.name,
-    username: user.username,
-    password: hashedPassword,
-    email: user.email,
-    birthDate: user.birthDate,
+  const token = jwt.sign({ _id: newUser._id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
   });
-
-  const token = jwt.sign(
-    { username: newUser.username },
-    process.env.SECRET_KEY,
-    {
-      expiresIn: "1d",
-    }
-  );
 
   return token;
 };
 
+const login = async (user) => {
+  const existingUser = await User.findOne({ email: user.email });
+  if (!existingUser) throw new ApiError(404, "User does not exist!");
+  const isPasswordCorrect = await bcrypt.compare(
+    user.password,
+    existingUser.password
+  );
+  if (!isPasswordCorrect) throw new ApiError(400, "Wrong password!");
+
+  const token = jwt.sign({ _id: existingUser._id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  const session = {
+    name: existingUser.name,
+    username: existingUser.username,
+    _id: existingUser._id,
+    token,
+  };
+  return session;
+};
+
 module.exports = {
   register,
+  login,
 };
